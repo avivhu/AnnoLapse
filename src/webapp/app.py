6e27f990b1cbbefd03ef48298801ae6cd0359192
Aviv Hurvitz <avivh@microsoft.com@microsoft.com>
2022-12-23 17:45:17 +0200

    Work on viewer web app

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -1,29 +1,90 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from azure.storage.blob import BlobServiceClient
+from markupsafe import escape
+import os
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+)
+
+
+def _get_container_client(container_name="thecontainer"):
+    connect_str = _get_connection_string()
+    service_client = BlobServiceClient.from_connection_string(connect_str)
+    client = service_client.get_container_client(container_name)
+    return client
+
+def _get_recorded_days():
+    client = _get_container_client()
+    prefix = "annolapse1/images/"
+    blobs = client.walk_blobs(name_starts_with=prefix)
+    days = list([b.name.split('/')[-2] for b in blobs])
+    days.sort()
+    return days
+
+
+def _get_recorded_series_in_day(day):
+    client = _get_container_client()
+    prefix = f"annolapse1/images/{day}/"
+    blobs = client.walk_blobs(name_starts_with=prefix)
+    series = list([b.name.split('/')[-2] for b in blobs])
+    series.sort()
+    return series
+
+
+def _get_images_in_series(day, series):
+    client = _get_container_client()
+    prefix = f"annolapse1/images/{day}/{series}/"
+    blobs = client.walk_blobs(name_starts_with=prefix)
+    images = list([b.name for b in blobs])
+    images.sort()
+    return images
+
+
 app = Flask(__name__)
 
+@app.route("/namer/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
 
-@app.route('/')
+@app.route("/days")
+def show_days():
+    days = _get_recorded_days()
+    days.sort(reverse=True)
+    aaa = '<br>\n'.join(days)
+    return f"Days:\n{aaa}"
+
+
+@app.route("/day/<day>")
+def show_day(day):
+    series = _get_recorded_series_in_day(day)
+    aaa = '<br>\n'.join(series)
+    return f"Series in day:\n{aaa}"
+
+
+@app.route("/apath/<path:subpath>")
+def showpath(subpath):
+    return f"Path, {escape(subpath)}!"
+
+@app.route("/")
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    print("Request for index page received")
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+@app.route("/test")
+def test():
+    print("testing")
+    days = _get_recorded_days()
+    series = _get_recorded_series_in_day(days[-1])
+    _get_images_in_series(days[-1], series[-1])
+    return " ".join(days)
+    # return render_template("hello.html", name=most_recent_blob.name)
+
+
+if __name__ == "__main__":
    app.run()
