0da1a7a39cbfa44907eb675c60ea9b9f3f8f3b78
Aviv Hurvitz <aviv.hurvitz@gmail.com>
2023-03-10 15:19:13 +0200

    Use ramdisk for temporary files

--- config.py
+++ config.py
@@ -3,7 +3,16 @@
 # Storage settings
 CONTAINER_NAME = "thecontainer"
 TIMELAPSE_NAME = "annolapse1"
-LOCAL_IMAGES_BASE_PATH = "/home/pi/timelapse_data"
+
+# Use save files to ramdisk to avoid wearing out the SD card.
+# They are uploaded and then deleted anyway so we only need temporary storage.
+# To set up ramdisk:
+#   sudo mkdir -p /media/ramdisk
+#   sudo mount -t tmpfs -o size=100M tmpfs /media/ramdisk
+#   sudo chmod 1777 /media/ramdisk
+#   grep /media/ramdisk /etc/mtab | sudo tee -a /etc/fstab
+# See also: https://askubuntu.com/questions/152868/how-do-i-make-a-ram-disk
+LOCAL_IMAGES_BASE_PATH = "/media/ramdisk/timelapse_data"
 
 # Capture settings
 CAMERA_ISO = 200
